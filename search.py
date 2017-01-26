4b20b72709624c069d46efffafdb32f61621936f
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2017-01-26 16:28:32 +0100

    Add search2, which first searches on code and then on repo
    This works better

--- search.py
+++ search.py
@@ -1,21 +1,22 @@
 from base64 import b64decode
 from github.MainClass import Github
 
-ignore_words = ('Mock', 'Test', '_test')
+ignore_words = ('Mock', 'Test', '_test', 'test_', 'test/', 'tests/')
 
 
 def is_interesting_result(result):
     for w in ignore_words:
-        if w in result.name:
+        if w in result.path:
             return False
-    return True
+    return result.repository.stargazers_count > 20
 
 
-def search(language, query, count):
+def search(token, language, query, count):
     """Search popular repo's for code"""
-    client = Github()
+    client = Github(token, user_agent='Sjord githubsearch')
     r = client.search_repositories(query='', sort='stars', order='desc', language=language, stars='>=100')
     for repo in r:
+        print(repo.full_name)
         r = client.search_code(query, language=language, size='>=100', repo=repo.full_name)
         for search_result in r:
             if is_interesting_result(search_result):
@@ -26,5 +27,24 @@ def search(language, query, count):
                     return
 
 
+def search2(token, language, query, count):
+    """Search popular repo's for code"""
+    client = Github(token, user_agent='Sjord githubsearch')
+    r = client.search_code(query, language=language, size='>=100')
+    for search_result in r:
+        if is_interesting_result(search_result):
+            print('FILE:', search_result.repository.full_name, search_result.path)
+            print(search_result.html_url)
+            # print(b64decode(search_result.content).decode('utf-8'))
+            count -= 1
+            if count == 0:
+                return
+
+
+
+
 if __name__ == "__main__":
-    search('php', 'session uniqid', 10)
+    with open('token.txt') as fp:
+        token = fp.read().strip()
+
+    search2(token, 'php', 'HTTP_X_FORWARDED_FOR', 20)
